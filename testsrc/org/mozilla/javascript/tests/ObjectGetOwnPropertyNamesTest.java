ebf1d295a3eb44e320a8c55ae7fac8a9ae4bd08a
Raphael Speyer <rspeyer@gmail.com>
2009-05-05 03:25:40 +1000

    partial implementation of getOwnPropertyNames

---
+++ testsrc/org/mozilla/javascript/tests/ObjectGetOwnPropertyNamesTest.java
@@ -0,0 +1,34 @@
+/*
+ * Tests for the Object.keys(obj) method
+ */
+package org.mozilla.javascript.tests;
+import junit.framework.TestCase;
+import org.mozilla.javascript.*;
+
+public class ObjectGetOwnPropertyNamesTest extends TestCase {
+
+  public void testShouldReturnAllPropertiesOfArg() {
+    NativeObject object = new NativeObject();
+    object.defineProperty("a", new NativeArray(0), ScriptableObject.EMPTY);
+    object.defineProperty("b", new NativeArray(0), ScriptableObject.DONTENUM);
+
+    Object result = eval("Object.keys(obj)", "obj", object);
+
+    NativeArray keys = (NativeArray) result;
+
+    assertEquals(2, keys.getLength());
+    assertEquals("a", keys.get(0, keys));
+    assertEquals("b", keys.get(1, keys));
+  }
+
+  private Object eval(String source, String id, Scriptable object) {
+    Context cx = ContextFactory.getGlobal().enterContext();
+    try {
+      Scriptable scope = cx.initStandardObjects();
+      scope.put(id, scope, object);
+      return cx.evaluateString(scope, source, "source", 1, null);
+    } finally {
+      Context.exit();
+    }
+  }
+}
