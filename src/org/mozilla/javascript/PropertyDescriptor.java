579f23c62b2cfbdd1f9ec51e3f2d0c8609358965
Raphael Speyer <rspeyer@gmail.com>
2009-05-11 05:28:20 +1000

    added more functionality to PropertyDescriptor

--- src/org/mozilla/javascript/PropertyDescriptor.java
+++ src/org/mozilla/javascript/PropertyDescriptor.java
@@ -1,73 +1,98 @@
 package org.mozilla.javascript;
 
 public class PropertyDescriptor implements Cloneable {
+  protected Boolean enumerable = null;
+  protected Boolean configurable = null;
   private Object value = null;
-  private Boolean enumerable = null;
   private Boolean writable = null;
-  private Boolean configurable = null;
-
   // TODO these two values might be mutable, is it still okay to clone?
   private Callable getter = null; 
   private Callable setter = null;
-
-  public PropertyDescriptor value(Object value) {
-    PropertyDescriptor copy = this.copy();
-    copy.value = value;
-    return copy;
-  }
 
   public PropertyDescriptor enumerable(boolean enumerable) {
     PropertyDescriptor copy = this.copy();
     copy.enumerable = enumerable;
     return copy;
   }
-
-  public PropertyDescriptor writable(boolean writable) {
-    PropertyDescriptor copy = this.copy();
-    copy.writable = writable;
-    return copy;
-  }
-
   public PropertyDescriptor configurable(boolean configurable) {
     PropertyDescriptor copy = this.copy();
     copy.configurable = configurable;
     return copy;
   }
+  public PropertyDescriptor value(Object value) {
+    if (this.isAccessorDescriptor()) 
+      throw new UnsupportedOperationException("Cannot add value to an accessor property descriptor");
 
-  public PropertyDescriptor getter(Callable getterFunction) {
     PropertyDescriptor copy = this.copy();
-    copy.getter = getterFunction;
+    copy.value = value;
     return copy;
   }
+  public PropertyDescriptor writable(boolean writable) {
+    if (this.isAccessorDescriptor()) 
+      throw new UnsupportedOperationException("Cannot add writable to an accessor property descriptor");
 
-  public PropertyDescriptor setter(Callable setterFunction) {
     PropertyDescriptor copy = this.copy();
-    copy.setter = setterFunction;
+    copy.writable = writable;
     return copy;
   }
+  public PropertyDescriptor getter(Callable getter) {
+    if (this.isDataDescriptor()) 
+      throw new UnsupportedOperationException("Cannot add getter to a data property descriptor");
 
-  public Object getValue() {
-    return this.value;
+    PropertyDescriptor copy = this.copy();
+    copy.getter = getter;
+    return copy;
+  }
+  public PropertyDescriptor setter(Callable setter) {
+    if (this.isDataDescriptor()) 
+      throw new UnsupportedOperationException("Cannot add setter to a data property descriptor");
+
+    PropertyDescriptor copy = this.copy();
+    copy.setter = setter;
+    return copy;
   }
 
   public boolean isEnumerable() {
     return enumerable;
   }
-
-  public boolean isWritable() {
-    return writable;
-  }
-
   public boolean isConfigurable() {
     return configurable;
   }
-
+  public Object getValue() {
+    return this.value;
+  }
+  public boolean isWritable() {
+    return writable;
+  }
   public Callable getGetter() {
     return getter;
   }
-
   public Callable getSetter() {
     return setter;
+  }
+
+  public boolean isDataDescriptor() {
+    return value != null || writable != null;
+  }
+  public boolean isAccessorDescriptor() {
+    return getter != null || setter != null;
+  }
+  public boolean isGenericDescriptor() {
+    return !isDataDescriptor() && !isAccessorDescriptor();
+  }
+
+  public NativeObject fromPropertyDescriptor() {
+    NativeObject obj = new NativeObject();
+    if (isDataDescriptor()) {
+      if (value != null) obj.defineProperty("value", value, ScriptableObject.EMPTY);
+      if (writable != null) obj.defineProperty("writable", writable, ScriptableObject.EMPTY);
+    } else if (isAccessorDescriptor()) {
+      if (getter != null) obj.defineProperty("get", getter, ScriptableObject.EMPTY);
+      if (setter != null) obj.defineProperty("set", setter, ScriptableObject.EMPTY);
+    }
+    if (enumerable != null) obj.defineProperty("enumerable", enumerable, ScriptableObject.EMPTY);
+    if (configurable != null) obj.defineProperty("configurable", configurable, ScriptableObject.EMPTY);
+    return obj;
   }
 
   private PropertyDescriptor copy() {
--- testsrc/org/mozilla/javascript/tests/es5/PropertyDescriptorTest.java
+++ testsrc/org/mozilla/javascript/tests/es5/PropertyDescriptorTest.java
@@ -1,12 +1,16 @@
 package org.mozilla.javascript.tests.es5;
+
 import junit.framework.TestCase;
 import org.mozilla.javascript.*;
+import java.util.Arrays;
 
 public class PropertyDescriptorTest extends TestCase {
-  private PropertyDescriptor desc;
+  private final PropertyDescriptor blank = new PropertyDescriptor();
+  private final Callable getter = new StubCallable();
+  private final Callable setter = new StubCallable();
 
   public void testShouldInitializeDataDescriptorThroughBuilderMethods() {
-    desc = new PropertyDescriptor().value("a").enumerable(true).writable(true).configurable(true);
+    PropertyDescriptor desc = blank.value("a").enumerable(true).writable(true).configurable(true);
     assertEquals("a", desc.getValue());
     assertEquals(true, desc.isEnumerable());
     assertEquals(true, desc.isWritable());
@@ -14,21 +18,93 @@ public class PropertyDescriptorTest extends TestCase {
   }
 
   public void testShouldInitialiseAccessorDescriptorThroughBuilderMethods() {
-    Callable getterFunc = new StubCallable();
-    Callable setterFunc = new StubCallable();
-    desc = new PropertyDescriptor().getter(getterFunc).setter(setterFunc).enumerable(true).configurable(true);
-    assertEquals(getterFunc, desc.getGetter());
-    assertEquals(setterFunc, desc.getSetter());
+    PropertyDescriptor desc = blank.getter(getter).setter(setter).enumerable(true).configurable(true);
+    assertEquals(getter, desc.getGetter());
+    assertEquals(setter, desc.getSetter());
     assertEquals(true, desc.isEnumerable());
     assertEquals(true, desc.isConfigurable());
   }
 
+  public void testShouldNotAllowGetterOrSetterToBeSetOnceValueOrWritableHaveBeenSet() {
+    for (PropertyDescriptor desc : Arrays.asList(blank.value("a"), blank.writable(true))) {
+      try {
+        desc.getter(getter);
+        fail("Descriptor cannot contain both value/writable and a getter");
+      } catch(UnsupportedOperationException ex) {
+        // this was expected
+      }
+      try {
+        desc.setter(setter);
+        fail("Descriptor cannot contain both value/writable and a setter");
+      } catch(UnsupportedOperationException ex) {
+        // this was expected
+      }
+    }
+  }
+
+  public void testShouldNotAllowValueOrWritableToBeSetOnceGetterOrSetterHaveBeenSet() {
+    for (PropertyDescriptor desc : Arrays.asList(blank.getter(getter), blank.setter(setter))) {
+      try {
+        desc.value("value");
+        fail("Descriptor cannot contain both getter/setter and a value");
+      } catch(UnsupportedOperationException ex) {
+        // this was expected
+      }
+      try {
+        desc.writable(true);
+        fail("Descriptor cannot contain both getter/setter and a writable");
+      } catch(UnsupportedOperationException ex) {
+        // this was expected
+      }
+    }
+  }
+
+  public void testShouldBeDataDescriptorOnlyWhenValueOrWritableIsSet() {
+    assertFalse(blank.isDataDescriptor());
+    assertFalse(blank.enumerable(true).isDataDescriptor());
+    assertFalse(blank.configurable(true).isDataDescriptor());
+    assertFalse(blank.getter(getter).isDataDescriptor());
+    assertFalse(blank.setter(setter).isDataDescriptor());
+    assertTrue(blank.value("a").isDataDescriptor());
+    assertTrue(blank.writable(true).isDataDescriptor());
+  }
+
+  public void testShouldBeAccessorDescriptorOnlyWhenGetterOrSetterIsSet() {
+    assertFalse(blank.isAccessorDescriptor());
+    assertFalse(blank.enumerable(true).isAccessorDescriptor());
+    assertFalse(blank.configurable(true).isAccessorDescriptor());
+    assertFalse(blank.value("a").isAccessorDescriptor());
+    assertFalse(blank.writable(true).isAccessorDescriptor());
+    assertTrue(blank.getter(getter).isAccessorDescriptor());
+    assertTrue(blank.setter(setter).isAccessorDescriptor());
+  }
+
+  public void testShouldBeGenericDescriptorOnlyWhenNotDataOrAccessorDescriptor() {
+    assertTrue(blank.isGenericDescriptor());
+    assertTrue(blank.enumerable(true).isGenericDescriptor());
+    assertTrue(blank.configurable(true).isGenericDescriptor());
+    assertFalse(blank.value("a").isGenericDescriptor());
+    assertFalse(blank.writable(true).isGenericDescriptor());
+    assertFalse(blank.getter(getter).isGenericDescriptor());
+    assertFalse(blank.setter(setter).isGenericDescriptor());
+  }
+
+  public void testFromPropertyDescriptorShouldCreateValidObjectForDataDescriptor() {
+    NativeObject expected = new NativeObject();
+    expected.defineProperty("value", Integer.valueOf(1), ScriptableObject.EMPTY);
+    expected.defineProperty("writable", Boolean.TRUE, ScriptableObject.EMPTY);
+    expected.defineProperty("enumerable", Boolean.TRUE, ScriptableObject.EMPTY);
+    expected.defineProperty("configurable", Boolean.TRUE, ScriptableObject.EMPTY);
+
+    PropertyDescriptor desc = new PropertyDescriptor().value(1).writable(true).enumerable(true).configurable(true);
+    NativeObject actual = desc.fromPropertyDescriptor();
+
+    assertEquals(expected.entrySet(), actual.entrySet());
+  }
 
   private static class StubCallable implements Callable {
     public Object call(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
       return null;
     }
   }
-
-
 }
